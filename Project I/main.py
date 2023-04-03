01c4ccfcf0a5e8c978ad588d694063d67af4b595
Alireza Saei <saeialireza359@gmail.com>
2023-04-04 02:31:32 +0430

    Read data

--- Project I/main.py
+++ Project I/main.py
@@ -1,7 +1,7 @@
-import json, csv
-import numpy as np
+import json
 import pandas as pd
 from tower import Tower
+
 
 def read_population(file):
     population_data = pd.read_csv(file, sep=',')
@@ -16,8 +16,8 @@ def read_config(file):
 
 if __name__ == '__main__':
     json_data = read_config('problem_config.txt')
-    print(json_data)
     tower = Tower(json_data)
+    print(str(tower))
 
     population = read_population('blocks_population.txt')
     print(population)
--- Project I/tower.py
+++ Project I/tower.py
@@ -1,6 +1,28 @@
 class Tower:
+    
     def __init__(self, json_data) -> None:
         self.cost = json_data['tower_construction_cost']
         self.maintenance = json_data['tower_maintanance_cost']
-        self.satisfaction_level = json_data['user_satisfaction_levels']
-        self.satisfaction_score = json_data['user_satisfaction_scores']
+        self.levels = json_data['user_satisfaction_levels']
+        self.scores = json_data['user_satisfaction_scores']
+
+    def __str__(self) -> str:
+        return f'''
+        ---------------Tower Configurations:---------------
+        Consturction Cost: {self.cost}$
+        Cost per 1MB/s: {self.maintenance}$
+        Customer Satisfaction Levels: {self.levels}
+        Customer Satisfaction Scores: {self.scores}
+        ---------------------------------------------------
+        '''
+
+    def get_score(self, bw):
+        if bw < self.levels[0]:
+            return 0.0
+        
+        if bw > self.levels[-1]:
+            return self.scores[-1]
+        
+        for i in range(1, len(self.levels)):
+            if self.levels[i-1] <= bw < self.levels[i]:
+                return self.scores[i-1]
