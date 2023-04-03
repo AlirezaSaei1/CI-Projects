1088ada415e93c9cd6a4c037342027bf27dc0568
Alireza Saei <saeialireza359@gmail.com>
2023-04-04 01:57:34 +0430

    Read data

--- Project I/main.py
+++ Project I/main.py
@@ -1,14 +1,23 @@
-import json
+import json, csv
+import numpy as np
+import pandas as pd
 from tower import Tower
 
 def read_population(file):
-    pass
+    population_data = pd.read_csv(file, sep=',')
+    return population_data.to_numpy()
+
 
 def read_config(file):
-    pass
+    with open(file) as f:
+        json_data = json.load(f)
+    return json_data
+
 
 if __name__ == '__main__':
     json_data = read_config('problem_config.txt')
+    print(json_data)
     tower = Tower(json_data)
 
     population = read_population('blocks_population.txt')
+    print(population)
