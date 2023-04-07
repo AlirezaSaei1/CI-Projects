3a9ab02fe8184d80096d67b377b388a0460e1af4
Alireza Saei <saeialireza359@gmail.com>
2023-04-07 14:49:41 +0430

    Moved non-code to data folder

--- Project I/main.py
+++ Project I/main.py
@@ -17,10 +17,10 @@ def read_config(file):
 
 
 if __name__ == '__main__':
-    cnf.load_config('problem_config.txt')
+    cnf.load_config('data/problem_config.txt')
     print(cnf.show_configurations())
 
-    population = read_population('blocks_population.txt')
+    population = read_population('data/blocks_population.txt')
     #print(population)
 
     shape = population.shape
