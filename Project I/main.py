5e98fa492ecf095a64c7a6939df83e5411de58c3
Alireza Saei <saeialireza359@gmail.com>
2023-04-12 21:31:23 +0430

    Fixed reading from file

--- Project I/main.py
+++ Project I/main.py
@@ -1,12 +1,17 @@
 import json
-import pandas as pd
+import numpy as np
 from config import Configurations as cnf
 from GA import GeneticAlgorithm
 
 
 def read_population(file):
-    population_data = pd.read_csv(file, sep=',', header=0)
-    return population_data.to_numpy()
+    population_data = []
+    with open(file) as f:
+        lines = f.readlines()
+    for line in lines:
+        population_data.append(list(map(int, line.split(','))))
+    
+    return np.array(population_data)
 
 
 def read_config(file):
@@ -19,10 +24,11 @@ if __name__ == '__main__':
     cnf.load_config('data/problem_config.txt')
     print(cnf.show_configurations())
 
+    # total population is = 191932
     population = read_population('data/blocks_population.txt')
     
     shape = population.shape
-    ga = GeneticAlgorithm(population, 10, max(cnf.levels)*10, shape[0], shape[1], 40)
+    ga = GeneticAlgorithm(population, 40, max(cnf.levels)*10000, shape[0], shape[1], 40)
     ga.mutate_population(ga.population)
     print('Population size:', len(ga.population))
 
