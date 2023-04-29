f3f9a682c63fa36773237fa586979068283790ca
Alireza Saei <saeialireza359@gmail.com>
2023-04-29 16:40:34 +0430

    Load cifar10 dataset and normalize them

--- Project II/main.py
+++ Project II/main.py
@@ -1,2 +1,12 @@
+import tensorflow as tf
+from tensorflow import keras
+
+# Get the CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
+# Normalize pixel values
+x_train = x_train.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
+
 if __name__ == '__main__':
     print('Project 2 Init')
