05a26658500ac7c1b454afa94b9264f47212ac3a
Alireza Saei <saeialireza359@gmail.com>
2023-05-09 01:29:30 +0430

    Using ResNet34 to extract features

--- Project II/main.py
+++ Project II/main.py
@@ -1,17 +1,28 @@
-import tensorflow as tf
-from tensorflow.keras.applications.resnet import ResNet34, preprocess_input
-from tensorflow.keras.datasets import cifar10
+from keras.datasets import cifar10
+from classification_models.keras import Classifiers
+
+# uncomment this to download the weights of resnet34 if you don't have the .h5 file (85MB)
+# import urllib.request
+# url = 'https://github.com/qubvel/classification_models/releases/download/0.0.1/resnet34_imagenet_1000.h5'
+# urllib.request.urlretrieve(url, 'resnet34_imagenet_1000.h5')
+
 
 # Get the CIFAR-10 dataset
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+(x_train_subset, y_train_subset), (x_test_subset, y_test_subset) = cifar10.load_data()
 
-x_train = preprocess_input(x_train)
-x_test = preprocess_input(x_test)
 
 # Load the pre-trained ResNet34 model
-resnet = ResNet34(include_top=False, weights='imagenet', input_shape=(32, 32, 3))
-resnet.trainable = False
+ResNet34, preprocess_input = Classifiers.get('resnet34')
+x_train = preprocess_input(x_train_subset)
+x_test = preprocess_input(x_test_subset)
+model = ResNet34(input_shape=(32, 32, 3), weights='resnet34_imagenet_1000.h5')
 
 
-if __name__ == '__main__':
-    print('Project 2 Init')
+# Extract features
+train_features = model.predict(x_train)
+test_features = model.predict(x_test)
+
+
+print('Train Features shape:', train_features)
+print('Test Features shape:', test_features.shape)
+
