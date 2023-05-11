6e0c3dbe8c7f9b575eb0cbd5a1bd8bb70232fd2d
Alireza Saei <saeialireza359@gmail.com>
2023-05-11 17:07:26 +0430

    Implemented Dense class functions

--- Project II/NeuralNetwork.py
+++ Project II/NeuralNetwork.py
@@ -1,22 +1,20 @@
 import numpy as np
 
-class NeuralNetwork:
-    def __init__(self):
-        pass
-
-
 class Dense:
     def __init__(self,n_inputs,n_neurons):
-        pass
-        # // To do: Define initial weight and bias
+        self.weights = np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
     
     def forward(self,inputs):
-        pass
-        # // To do: Define input and output
+        self.inputs = inputs
+        self.outputs = np.dot(self.inputs, self.weights) + self.biases
+        return self.outputs
 
     def backward(self,b_input):
-        pass
-        # // To do: Weight and bias gradients
+        self.weights_gradient = np.dot(self.inputs.T, b_input)
+        self.biases_gradient = np.sum(b_input, axis=0, keepdims=True)
+        b_output = np.dot(b_input, self.weights.T)
+        return b_output
 
 
 class Sigmoid:
