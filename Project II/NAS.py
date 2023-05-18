df4c130c5511fb694a454ba8504179a92ec7054c
Mohammad <tavakolimohammad969@gmail.com>
2023-05-18 23:18:57 +0330

    gitignore updated, fix error

--- Project II/NAS.py
+++ Project II/NAS.py
@@ -1,50 +1,44 @@
 import random
 
-activations = ['ReLU', 'Sigmoid']
-learning_rates = [0.1, 0.01, 0.001, 0.0001]
+activations_all = ['ReLU', 'Sigmoid']
+learning_rates_all = [0.1, 0.01, 0.001, 0.0001]
+
 
 class NAS:
     def __init__(self) -> None:
         pass
-
 
     def random_network(self):
         # setup
         feature_extractor = ['ResNet18', 'ResNet34', 'Vgg11']
         num_layers = random.choice([1, 2, 3])
         layer_sizes = [random.choice([10, 20, 30]) for _ in range(num_layers)]
-        activations = [random.choice(activations) for _ in range(num_layers)]
-        learning_rate = random.choice(learning_rates)
+        activations = [random.choice(activations_all) for _ in range(num_layers)]
+        learning_rate = random.choice(learning_rates_all)
 
         # TODO: neural network construction
         network = None
 
         return network
-    
 
     def evaluate(self, network):
         pass
 
-
     def selection(self):
         pass
-
 
     def crossover(self):
         pass
 
-
     def mutation(self):
         pass
-
 
     def replacement(self):
         pass
 
-
     def run(self, generations=10):
         for generation in range(generations):
-            print(f'--- Generation {generation+1} ---')
+            print(f'--- Generation {generation + 1} ---')
 
             # TODO: selection
             parents = []
