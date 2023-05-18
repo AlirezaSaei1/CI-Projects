e9369f504b7fdb996e7cef9dd70fd23e7df3128e
Alireza Saei <saeialireza359@gmail.com>
2023-05-18 17:58:44 +0430

    Added template code, functions to complete

--- Project II/NAS.py
+++ Project II/NAS.py
@@ -7,7 +7,8 @@ class NAS:
     def __init__(self) -> None:
         pass
 
-    def random_network():
+
+    def random_network(self):
         # setup
         layer_size = random.choice(range(128))
         activation = random.choice(activations)
@@ -17,4 +18,46 @@ class NAS:
         network = None
 
         return network
+    
 
+    def evaluate(self, network):
+        pass
+
+
+    def selection(self):
+        pass
+
+
+    def crossover(self):
+        pass
+
+
+    def mutation(self):
+        pass
+
+
+    def replacement(self):
+        pass
+
+
+    def run(self, generations=10):
+        for generation in range(generations):
+            print(f'--- Generation {generation+1} ---')
+
+            # TODO: selection
+            parents = []
+
+            # TODO: crossover
+            offspring = []
+
+            # TODO: mutation
+            offspring = []
+
+            # TODO: eval
+            fitness = []
+
+            # TODO: replacement
+            population = []
+
+        report = self.evaluate(population[0])
+        print(f'Report: {report}')
