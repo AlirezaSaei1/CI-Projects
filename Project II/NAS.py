69562e47f30e31fe635d40698c6de07ebf0cbe6c
Alireza Saei <saeialireza359@gmail.com>
2023-05-18 17:50:09 +0430

    Added NAS.py for part II of project

---
+++ Project II/NAS.py
@@ -0,0 +1,20 @@
+import random
+
+activations = ['relu', 'sigmoid']
+learning_rates = [0.1, 0.01, 0.001, 0.0001]
+
+class NAS:
+    def __init__(self) -> None:
+        pass
+
+    def random_network():
+        # setup
+        layer_size = random.choice(range(128))
+        activation = random.choice(activations)
+        learning_rates = random.choice(learning_rates)
+
+        # TODO: neural network construction
+        network = None
+
+        return network
+
