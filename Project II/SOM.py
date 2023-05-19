d303ca3a1b35ea5062854b6cb4488a8181a24622
Alireza Saei <saeialireza359@gmail.com>
2023-05-19 11:26:23 +0430

    Added neighbor function of SOM

--- Project II/SOM.py
+++ Project II/SOM.py
@@ -2,8 +2,18 @@ import numpy as np
 
 
 class SOM:
-    def __init__(self, input_size=512, output_size=10):
-        self.input_size = input_size
-        self.output_size = output_size
-        self.weights = np.random.randn(*output_size, input_size)
+    def __init__(self, x, y, input_dim, sigma=1.0, lr=0.1):
+        self.x = x
+        self.y = y
+        self.input_dim = input_dim  
+        self.sigma = sigma  
+        self.lr = lr
+        self.weights = np.random.rand(x, y, input_dim) 
+
+    def neighborhood(self, c, r):
+        d = np.sqrt((np.arange(self.x) - c)**2 + (np.arange(self.y) - r)**2)
+        return np.exp(-(d**2) / (2 * self.sigma**2))
         
+
+    def train():
+        pass
